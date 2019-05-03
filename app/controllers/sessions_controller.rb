a516768aebdfd10b68dae7223c2706684c40249a
Alekseev Aleksandr <alekseev2002@inbox.ru>
2019-05-03 11:05:26 +0300

    Fixed some UI issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if current_user
       redirect_to event_index_url
     end
-    user = User.find_by_username(params[:email])
+    user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
     else
