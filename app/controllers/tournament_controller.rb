053fc671a7e438b657795229f9d340c19114ba71
Alekseev Aleksandr <alekseev2002@inbox.ru>
2019-06-06 01:17:29 +0300

    Changed tournament table

--- app/controllers/tournament_controller.rb
+++ app/controllers/tournament_controller.rb
@@ -2,38 +2,13 @@ class TournamentController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :set_tournament, only: [:show]
   before_action :check_logged_in
+  include ChallengeHelper
 
   def index
   end
 
   def show
-    # Same as in challenge controller. How to merge?
-    challenges = @tournament.challenges
-    @challenges_data = challenges.map { |x|
-      cur_item = {
-          id: x.id,
-          player1: Submission.find(x.sub1).name,
-          player2: Submission.find(x.sub2).name,
-          player1_verdict: x.player_1_verdict,
-          player2_verdict: x.player_2_verdict,
-      }
-      if x.winner.nil?
-        if x.is_draw
-          cur_item[:winner] = 'Ничья'
-        else
-          cur_item[:winner] = 'N/A'
-        end
-      else
-        cur_item[:winner] = Submission.find(x.winner).name
-      end
-
-      if x.status == 'Running'
-        cur_item[:status] = 'Тестируется'
-      else
-        cur_item[:status] = 'Протестировано'
-      end
-      cur_item
-    }
+    @challenges_data = get_data_for_index @tournament.challenges
   end
 
   def new
