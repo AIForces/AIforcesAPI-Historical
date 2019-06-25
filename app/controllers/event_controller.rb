eb36b1578a34fe5abd08643b1ad4ca4d1dc4fe94
Alekseev Aleksandr <alekseev2002@inbox.ru>
2019-06-26 00:48:17 +0300

    new api for static pages

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -11,4 +11,22 @@ class EventController < ApplicationController
     @statement_html = File.read("storage/games/#{g.name}/#{g.statement_file}")
   end
 
+  def rules_spa
+    rules_html = File.read("storage/events/#{current_event.name}/#{current_event.rules_file}")
+    render inline: rules_html, layout: false
+  end
+
+  def statements_spa
+    g = current_event.game
+    statements_html = File.read("storage/games/#{g.name}/#{g.statement_file}")
+    render inline: statements_html, layout: false
+  end
+
+  def visualizer_spa
+    vis_html = File.read("storage/games/tron/visualizer/visualizer.html")
+    vis_css = File.read("storage/games/tron/visualizer/visualizer.css")
+    vis_js = File.read("storage/games/tron/visualizer/visualizer.js")
+
+    render inline: "#{vis_html}<style>#{vis_css}</style><script>#{vis_js}</script>"
+  end
 end
