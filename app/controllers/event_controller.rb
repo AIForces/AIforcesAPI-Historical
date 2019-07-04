4b7cdfaf4b015516219e7efa5abf2b55afb9acba
Alekseev Aleksandr <alekseev2002@inbox.ru>
2019-07-04 12:14:19 +0300

    Send json separately

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -12,14 +12,22 @@ class EventController < ApplicationController
   end
 
   def rules_spa
-    rules_html = File.read("storage/events/#{current_event.name}/#{current_event.rules_file}")
-    render inline: rules_html, layout: false
+    html = File.read("storage/events/#{current_event.name}/#{current_event.rules_file}")
+    render render json: {
+        html: html,
+        json: ""
+    }
   end
 
   def statements_spa
     g = current_event.game
-    statements_html = File.read("storage/games/#{g.name}/#{g.statement_file}")
-    render inline: statements_html, layout: false
+    html = File.read("storage/games/#{g.name}/#{g.statement_file}")
+    css = File.read("storage/games/#{g.name}/statement.css")
+    js = File.read("storage/games/#{g.name}/statement.js")
+    render json: {
+        html: "#{html}<style>#{css}</style>",
+        json: js
+    }
   end
 
   def visualizer_spa
@@ -27,7 +35,10 @@ class EventController < ApplicationController
     vis_css = File.read("storage/games/tron/visualizer/visualizer.css")
     vis_js = File.read("storage/games/tron/visualizer/visualizer.js")
 
-    render inline: "#{vis_html}<style>#{vis_css}</style><script>#{vis_js}</script>"
+    render json: {
+        html: "#{vis_html}<style>#{vis_css}</style>",
+        json: vis_js
+    }
   end
 
   def participants
