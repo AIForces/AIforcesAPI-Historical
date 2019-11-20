d728adb1db8eaf7522fd9be5072e8272c1e9b642
Alekseev Aleksandr <alekseev2002@inbox.ru>
2019-11-20 19:59:16 +0300

    Deleted old routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,4 @@
 Rails.application.routes.draw do
-  get 'challenge/new'
-  get 'challenge/index'
-  get 'challenge/manage'
-  get 'challenge/rejudge'
-  get 'challenge/destroy'
-  get 'challenge/log'
-  get 'challenge/visualize'
-  match 'challenge/create' => 'challenge#create', via: :post, as: :challenge_create
-  match 'challenge/receive_data' => 'challenge#receive_data', via: :post, as: :challenge_receive_data
-  match 'challenge/update_status' => 'challenge#update_status', via: :post, as: :challenge_update_status
-
-  get 'event/index'
-  get 'event/rules'
-  get 'event/statements'
-
-  match 'judge/update_status' => 'judge#update_status', via: :post, as: :judge_update_status
-
-  get 'submissions/index'
-  get 'submissions/new'
-  get 'submissions/make_used_for_tours'
-  get 'submissions/source'
-  get 'submissions/manage'
-  get 'submissions/destroy'
-  get 'submissions/make_opened'
-  match "submissions/create" => "submissions#create", :via => :post, :as => :submissions_create
-
-  get 'tournament/index'
-  get 'tournament/new'
-  get 'tournament/show'
-  match "tournament/create" => "tournament#create", :via => :post, :as => :tournament_create
-
-  get 'help', to: 'staticpages#help'
-
-  get 'login', to: 'sessions#new', as: :sessions_new
-  get 'logout', to: 'sessions#destroy', as: :sessions_destroy
-  match "auth" => "sessions#create", :via => :post, :as => :sessions_create
-
-  get 'users/new', to: "users#new", as: :users_new
-  match "users/create" => "users#create", :via => :post, :as => :users_create
-
-  root :to => redirect('/login')
-
-  # New API for SPA frontend
-
   get 'api/submissions/', to: 'submissions#index_spa'
   get 'api/submissions/:id', to: 'submissions#show_spa'
   get 'api/submissions/public/:id', to: 'submissions#public'
